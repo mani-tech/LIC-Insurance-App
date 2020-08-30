45a91649e60c32d60dc033ee4c3c95c6c946be1c
mani-tech <devarakondamaniteja112@gmail.com>
2020-08-30 06:48:32 +0530

    first commit to repository

---
+++ src/main/java/com/mani/LICInsuranceApp/LicInsuranceAppApplication.java
@@ -0,0 +1,13 @@
+package com.mani.LICInsuranceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicInsuranceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicInsuranceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mani/LICInsuranceApp/LicInsuranceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mani.LICInsuranceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicInsuranceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
